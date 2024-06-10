6fe2cafdb244fd83792a464dfeb5f9d4e1ab38f7
Joseph Otoo <38560123+Joseph1Otoo@users.noreply.github.com>
2024-06-10 01:44:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,27 @@ from sklearn.preprocessing import StandardScaler
 model = xgb.XGBClassifier()
 model.load_model('xgb_fetal_model.json')
 
+# Feature names
+feature_names = [
+    'baseline value', 'accelerations', 'fetal_movement',
+    'uterine_contractions', 'light_decelerations', 'severe_decelerations',
+    'prolongued_decelerations', 'abnormal_short_term_variability',
+    'mean_value_of_short_term_variability',
+    'percentage_of_time_with_abnormal_long_term_variability',
+    'mean_value_of_long_term_variability', 'histogram_width',
+    'histogram_min', 'histogram_max', 'histogram_number_of_peaks',
+    'histogram_number_of_zeroes', 'histogram_mode', 'histogram_mean',
+    'histogram_median', 'histogram_variance', 'histogram_tendency'
+]
+
 # Define Streamlit app
 st.title('Fetal Health State Prediction')
 st.write('Enter the fetal variables to predict the outcome')
 
 # Define the input fields
 inputs = []
-for i in range(21):  # Assuming there are 21 features
-    inputs.append(st.number_input(f'Feature {i+1}', value=0.0))
+for feature in feature_names:
+    inputs.append(st.number_input(feature, value=0.0))
 
 if st.button('Predict'):
     input_data = np.array(inputs).reshape(1, -1)
